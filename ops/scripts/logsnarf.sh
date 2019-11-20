205f5333638e101bbea8ff5a4dc2348d5e1daa28
Paul Sadauskas <psadauskas@gmail.com>
2019-11-19 20:32:52 -0700

    Configured packer to use DO

--- ops/scripts/logsnarf.sh
+++ ops/scripts/logsnarf.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -Eeuxo pipefail
+
+CENTOS=`rpm --eval '%{centos_ver}'`
+
+yum update -y
+yum groupinstall -y "Development Tools"
+yum install -y git wget
 
 wget -q https://github.com/caddyserver/caddy/releases/download/v2.0.0-beta9/caddy2_beta9_linux_amd64
 install -o root -g root -m 755 -v caddy2_beta9_linux_amd64 /usr/local/bin/caddy
@@ -18,22 +24,20 @@ install -o root -g root -m 644 -v -D /tmp/templates/Caddyfile /etc/caddy2/Caddyf
 install -o root -g logsnarf -m 0770 -v -d /etc/ssl/caddy2
 install -o logsnarf -g logsnarf -m 555 -v -D -d /var/www
 
-# cp -R example.com /var/www/
-# chown -R www-data:www-data /var/www/example.com
-# chmod -R 555 /var/www/example.com
-
 install -o root -g root -m 644 /tmp/templates/caddy2.service /etc/systemd/system/caddy2.service
 
 systemctl daemon-reload
 systemctl enable caddy2.service
 systemctl start caddy2.service
 
-# dnf install -y https://copr-be.cloud.fedoraproject.org/results/psadauskas/ruby-install/fedora-29-x86_64/01028211-ruby-install/ruby-install-0.7.0-1.noarch.rpm
-dnf install -y /tmp/templates/ruby-2.6.5-1.el8.x86_64.rpm
-gem install bundler
+if [[ "$CENTOS" == "8" ]]; then
+  # dnf install -y https://copr-be.cloud.fedoraproject.org/results/psadauskas/ruby-install/fedora-29-x86_64/01028211-ruby-install/ruby-install-0.7.0-1.noarch.rpm
+  dnf install -y /tmp/templates/ruby-2.6.5-1.el8.x86_64.rpm
+else
+  yum install -y https://github.com/feedforce/ruby-rpm/releases/download/2.6.5/ruby-2.6.5-1.el7.centos.x86_64.rpm
+fi
 
-dnf groupinstall -y "Development Tools"
-dnf install -y git
+gem install bundler
 
 cd /var/www
 
