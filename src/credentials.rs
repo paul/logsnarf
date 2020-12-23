158bfa659bc57f59addc6c511313e80262c29fac
Paul Sadauskas <psadauskas@gmail.com>
2020-12-23 16:23:25 -0700

    Get fetching creds from dynamodb working

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,20 +1,16 @@
 use std::collections::HashMap;
 use std::default::Default;
 
-use async_std::task;
-use dynomite::{Item, FromAttributes};
-use dynomite::dynamodb::{DynamoDb, DynamoDbClient, GetItemInput, GetItemError};
-use rusoto_core::Region;
+// use dynomite::dynamodb::{DynamoDb, GetItemError, GetItemInput};
+// use dynomite::{Attributes, FromAttributes, Item};
+use dynomite::{
+    dynamodb::{DynamoDb, GetItemError, GetItemInput},
+    Attributes, Item, FromAttributes
+};
 use thiserror::Error;
-// use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, GetItemInput};
-// use rusoto_dynamodb::{DynamoDbClient};
-
-use crate::settings::Settings;
-
 
 #[derive(Debug, Error)]
-pub enum CredentialsError
-{
+pub enum CredentialsError {
     #[error("No credentials for token {0}")]
     MissingCredentials(String),
 
@@ -27,53 +23,70 @@ pub enum CredentialsError
     DynamoDbError(#[from] rusoto_core::RusotoError<GetItemError>),
 }
 
-#[derive(Item, Clone)]
+#[derive(Item, Clone, Debug)]
 pub struct Credentials {
     #[dynomite(partition_key)]
-    token: String,
-    name: String,
-    adapter: String,
-    creds: HashMap<String, String>,
+    pub token: String,
+    pub name: String,
+
+    pub credentials: Secrets,
 }
 
-pub struct Store {
-    settings: Settings,
-    dynamo_db: rusoto_dynamodb::DynamoDbClient,
-    cache: HashMap<String, Credentials>,
+#[derive(Attributes, Clone, Debug)]
+#[dynomite(tag = "adapter")]
+pub enum Secrets {
+    #[dynomite(rename = "influxdb_v1")]
+    InfluxDbCredentials(InfluxDbCredentials)
 }
 
-impl Store {
-    pub fn new(settings: Settings) -> Store {
-        Store {
-            settings,
-            dynamo_db: DynamoDbClient::new(Region::UsEast2),
-            cache: HashMap::with_capacity(100),
-        }
-    }
 
-    pub fn fetch(&mut self, token: &String) -> Result<Credentials,CredentialsError> {
-        match self.cache.get_mut(token) {
-            Some(creds) => Ok(creds.clone()),
-            None => {
-                task::block_on(async {
-                    let creds = self.get_item(token).await?;
-                    self.cache.insert(token.clone(), creds.clone());
-                    Ok(creds)
-                })
-            }
-        }
-    }
+#[derive(Attributes, Clone, Debug)]
+pub struct InfluxDbCredentials {
+    #[dynomite(rename = "type")]
+    pub adapter: String,
+    pub influxdb_url: String,
+}
 
-    async fn get_item(&self, token: &String) -> Result<Credentials,CredentialsError> {
-        let req = GetItemInput {
-            table_name: self.settings.credentials_table.clone().into(),
-            key: CredentialsKey { token: token.clone() }.into(),
-            ..Default::default()
-        };
-        self.dynamo_db.get_item(req).await?.item
-            .map(|item| Credentials::from_attrs(item)
+pub async fn fetch(client: &dyn DynamoDb, table_name: String, token: &String) -> Result<Credentials, CredentialsError> {
+    let req = GetItemInput {
+        table_name,
+        key: CredentialsKey { token: token.clone() }.into(),
+        ..Default::default()
+    };
+    client.get_item(req).await?.item
+        .map(|item| {
+            println!("{:#?}", item);
+            Credentials::from_attrs(&mut item.clone())
                 .map_err(|source| CredentialsError::MalformedCredentials { source })
-                )
-            .ok_or(CredentialsError::MissingCredentials(token.clone()))?
+        })
+        .ok_or(CredentialsError::MissingCredentials(token.clone()))?
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio_test::assert_ok;
+
+    use dynomite::dynamodb::{DynamoDbClient};
+    use rusoto_core::Region;
+
+    #[tokio::test]
+    async fn test_fetch() {
+        let client = DynamoDbClient::new(Region::UsEast2);
+        let table = "logsnarf_config".to_string();
+        let token = "e0ff2e6751893dcd7fcb7a94d4535437".to_string();
+        let result = fetch(&client, table, &token).await;
+        println!("{:#?}", result);
+        // assert_ok!(result);
+        let creds = result.unwrap();
+        assert_eq!(creds.token, token);
+        match creds.credentials {
+            Secrets::InfluxDbCredentials(secrets) => {
+                assert_eq!(secrets.adapter, "influxdb_v1".to_string());
+                assert_eq!(secrets.influxdb_url, "http://localhost:8086/logsnarf".to_string());
+            },
+            _ => assert!(false, "secrets wasn't an InfluxDbCredentials")
+
+        }
     }
 }
