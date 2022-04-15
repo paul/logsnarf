d9009dd565d0973fdb22bd3ebe833c9e1712ac41
Paul Sadauskas <psadauskas@gmail.com>
2022-04-15 16:48:50 -0600

    Added different logging for local vs lambda

--- src/bin/handle_log.rs
+++ src/bin/handle_log.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use std::sync::RwLock;
 
 use tracing::info;
 
 use lambda_http::{
-    service_fn, Request,
-    Response,
+    service_fn,
+    Request, RequestExt,
+    Response, IntoResponse,
     http::StatusCode};
 
 use tokio::signal::unix::{signal, SignalKind};
@@ -15,11 +17,11 @@ use logsnarf::utils;
 type E = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 type Token = String;
-type Entry = Vec<u8>;
+type Metric = Vec<u8>;
 
 #[derive(Default)]
 struct Buffer {
-    pub data: RwLock<HashMap<Token, Entry>>,
+    pub data: RwLock<HashMap<Token, Vec<Metric>>>,
 }
 
 #[tokio::main]
@@ -29,28 +31,41 @@ async fn main() -> Result<(), E> {
     let buffer = Buffer::default();
     let buffer_ref = &buffer;
 
-    let mut shutdown = signal(SignalKind::terminate())?;
-
-    let event_func = move |event: Request| async move {
-        let (parts, body) = event.into_parts();
-
-        let token = parts.uri.path().split("/").last().unwrap();
-
-        info!("Writing: {} {:?}", token, body);
-        buffer_ref.data.write().unwrap().insert(token.to_string(), body.to_vec());
-
-        Ok(Response::builder().status(StatusCode::ACCEPTED).body(()).unwrap())
+    let mut shutdown = if utils::local_lambda() {
+        signal(SignalKind::interrupt())?
+    } else {
+        signal(SignalKind::terminate())?
     };
 
-    // lambda_http::run(service_fn(event_func)).await?;
     tokio::select! {
-        _ = lambda_http::run(service_fn(event_func)) => {},
+        _ = lambda_http::run(service_fn(move |event: Request| handle_event(buffer_ref, event))) => {},
         _ = shutdown.recv() => {
-            info!("Flushing: {:?}", buffer_ref.data);
-            buffer_ref.data.write().unwrap().clear();
+            flush_all(&buffer_ref).await?
         },
     }
 
     Ok(())
 }
 
+async fn handle_event(buffer: &Buffer, req: Request) -> Result<impl IntoResponse, E> {
+    let _context = req.lambda_context();
+    let (parts, body) = req.into_parts();
+
+    let token = parts.uri.path().split("/").last().unwrap();
+
+    info!("Writing: {} {:?}", token, body);
+    // buffer.data.write().unwrap().insert(token.to_string(), body.to_vec());
+    let mut data = buffer.data.write().unwrap();
+    match data.entry(token.to_string()) {
+        Entry::Occupied(mut e) => { e.get_mut().push(body.to_vec()); },
+        Entry::Vacant(e) => {e.insert(vec![body.to_vec()]); },
+    };
+
+    Ok(Response::builder().status(StatusCode::ACCEPTED).body(()).unwrap())
+}
+
+async fn flush_all(buffer: &Buffer) -> Result<(), E> {
+    info!("Flushing: {:?}", buffer.data);
+    buffer.data.write().unwrap().clear();
+    Ok(())
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,29 @@
+use tracing_subscriber::{prelude::*, Layer};
+
 /// Setup tracing
 pub fn setup_tracing() {
-    let subscriber = tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_ansi(false)
-        // CloudWatch adds the ingestion time.
-        .without_time()
-        .finish();
+    let env_log = tracing_subscriber::fmt::layer()
+        .with_filter(tracing_subscriber::EnvFilter::from_default_env());
+    let subscriber = tracing_subscriber::Registry::default().with(env_log);
+
+    let host_subscriber = if local_lambda() {
+        None
+    } else {
+        Some(
+            tracing_subscriber::fmt::layer()
+                .with_ansi(false)
+                .without_time(),
+        )
+    };
+
+    let subscriber = subscriber.with(host_subscriber);
+
     tracing::subscriber::set_global_default(subscriber).expect("failed to set tracing subscriber");
+}
+
+pub fn local_lambda() -> bool {
+    match std::env::var("TERM") {
+        Ok(_) => true,
+        Err(_) => false,
+    }
 }
