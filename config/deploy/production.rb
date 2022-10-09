d0e38a7df6c6531eb08aa8b1bd374d42e796a566
Paul Sadauskas <psadauskas@gmail.com>
2022-10-09 14:08:41 -0600

    Fixed credentials struct

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "logsnarf-rb-new.logsnarf.com", user: "root", roles: %w[app]
+server "do.logsnarf.com", user: "root", roles: %w[app]
 
 # role-based syntax
 # ==================
--- lib/credentials.rb
+++ lib/credentials.rb
@@ -8,8 +8,8 @@ class Credentials < Dry::Struct
 
   attribute :name, Types::String
   attribute :token, Types::String
-  attribute :created_at, Types::JSON::Time
-  attribute :updated_at, Types::JSON::Time
+  attribute? :created_at, Types::JSON::Time
+  attribute? :updated_at, Types::JSON::Time
 
   attribute :credentials do
     attribute :type, Types::String
